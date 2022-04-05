78c0e4e662c8eaabcb61a94a8c32e344e7f3e05c
Rozenberg-jv <avangard.npaper@gmail.com>
2022-04-05 21:15:15 +0300

    task

---
+++ src/com/company/stud/lessons/les12/task02/IntListSumTask.java
@@ -0,0 +1,35 @@
+package com.company.stud.lessons.les12.task02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class IntListSumTask {
+
+  public static void main(String[] args) {
+
+    List<Integer> list1 = Arrays.asList(1, 5, 3);
+    List<Integer> list2 = Arrays.asList(2, -5, 4, 10, 15);
+
+    List<Integer> result = sumLists(list1, list2);
+    System.out.println(result);
+  }
+
+  private static List<Integer> sumLists(List<Integer> list1, List<Integer> list2) {
+
+    List<Integer> result = new ArrayList<>();
+
+    int minSize = Math.min(list1.size(), list2.size());
+
+    for (int i = 0; i < minSize; i++) {
+      result.add(list1.get(i) + list2.get(i));
+    }
+
+    result.addAll(list1.size() > list2.size() ?
+      list1.subList(minSize, list1.size()) :
+      list2.subList(minSize, list2.size()));
+
+    return result;
+  }
+
+}
